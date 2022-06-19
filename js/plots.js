9086e18c80b01d3c1154dc76c34ee633f701f663
Izzy Diaz III <Izzy.DiazIII@lumentum.com>
2022-06-19 10:51:59 -0700

    line chart practice

--- js/plots.js
+++ js/plots.js
@@ -1,1 +1,12 @@
 Plotly.newPlot("plotArea", [{x: [1, 2, 3], y: [10, 20, 30]}]);
+// create a new line chart of my own, with a more consistent call via variables
+// pass the variable instead of the exact array with key-values (refactor the above plot)
+var trace = {
+    x: [1,2,3],
+    y: [10,20,30],
+    type: "line"
+};
+var layout = {
+    title: "First Line Chart via Js + Plotly!"
+};
+Plotly.newPlot("plotArea", [trace], [layout]);
